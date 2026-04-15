0e60287dab465a18b24e767b5bc679bd28c9f722
LuisDaniel15 <144951758+LuisDaniel15@users.noreply.github.com>
2026-04-15 15:26:25 +0000

    voliviendo a supabase

--- guarderia-v1/app/config/db_config.py
+++ guarderia-v1/app/config/db_config.py
@@ -3,16 +3,30 @@ import os
 
 def get_db_connection():
     try:
-        # Tu nueva URL de Neon
-        # El parámetro sslmode=require es obligatorio en Neon
-        db_url = "postgresql://neondb_owner:npg_Bivq23cHdnLe@ep-misty-glade-amirk0iq-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require"
-        
-        conn = psycopg2.connect(db_url)
+        conn = psycopg2.connect(
+            host="aws-0-us-west-2.pooler.supabase.com",
+            port=6543,  # 🔥 ESTE ES EL CAMBIO
+            database="postgres",
+            user="postgres.ilbbknzbfbmzpzineojv",
+            password="zPPQYrCSnhAHd1KE"
+        )
         return conn
     except Exception as e:
-        print("\n==================================================")
-        print(f"❌ ERROR DE CONEXIÓN EN NEON: {e}")
-        print("==================================================\n")
+        print(f"Error: {e}")
         return None
 
-    # Supabase3115*
+# CONEXION A LA BASE DE DATOS DE RESPALDO EN NEON
+# import psycopg2
+# import os
+
+# def get_db_connection():
+#     try:
+#         db_url = "postgresql://neondb_owner:npg_Bivq23cHdnLe@ep-misty-glade-amirk0iq-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require"
+        
+#         conn = psycopg2.connect(db_url)
+#         return conn
+#     except Exception as e:
+#         print("\n==================================================")
+#         print(f"❌ ERROR DE CONEXIÓN EN NEON: {e}")
+#         print("==================================================\n")
+#         return None
